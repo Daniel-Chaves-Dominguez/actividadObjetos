e97cd7492c61ab6ada3fd058ec091b094c1123a2
Daniel Chaves Domínguez <dchaves260299@gmail.com>
2025-11-17 12:22:12 +0100

    change in Main

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,7 @@ import Artículos.albumMusica;
 import Usuarios.Administrador;
 import Usuarios.Cliente;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         //Crea 2 artículos de cada tipo
